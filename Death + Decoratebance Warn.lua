98d06d098c45642829fb376eaa088c272bc4ade4
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-05-18 17:43:11 +0800

    Create Death + Decoratebance Warn.lua

---
+++ Death + Decoratebance Warn.lua
@@ -0,0 +1,7 @@
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Spawn"
+	cue2.SoundId = "rbxassetid://1318185544"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 0.5
+	cue2:Play()
