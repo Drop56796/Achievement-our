09cccce2547ed88d51eeaa0ba391770f37e36506
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-05-18 15:09:31 +0800

    Create Decoratebance.lua

---
+++ Decoratebance.lua
@@ -0,0 +1,10 @@
+---====== Load achievement giver ======---
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Decnbance give you out",
+    Desc = "Hola for you and don't be to slow",
+    Reason = "Survive the Decoratebance",
+    Image = "rbxassetid://12583460254"
+})
