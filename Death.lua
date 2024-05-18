0428beaec41046922c8aa1d51824b4187fd4b6f7
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-05-18 15:06:38 +0800

    Create Death.lua

---
+++ Death.lua
@@ -0,0 +1,10 @@
+---====== Load achievement giver ======---
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Why are you doing this to me?",
+    Desc = "You want Did",
+    Reason = "Survive the Death",
+    Image = "rbxassetid://10826375568"
+})
